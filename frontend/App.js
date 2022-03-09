c2ba1aa2f608e6ef58fab8e86a9256bbdd10506a
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-09 11:17:15 -0800

    created useDarkMode hook in seperate file.

--- frontend/App.js
+++ frontend/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import useDarkMode from "./components/hooks/useDarkMode";
 
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
@@ -8,7 +9,7 @@ import "./style.css";
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
     axios
