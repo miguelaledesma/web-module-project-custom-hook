608ddba8c33be7bc3534ae89dd760dd0ad41fd3c
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-09 12:17:09 -0800

    added custom component for localstorage

---
+++ frontend/components/hooks/useLocalStorage.js
@@ -0,0 +1,54 @@
+
+import { useState } from 'react'; 
+
+
+
+const useLocalStorage = (key, initialValue) => {
+
+const [state, setState] = useState(() => {
+    if(localStorage.getItem(key)){
+        return JSON.parse(localStorage.getItem(key));
+    }
+
+    localStorage.setItem(key, JSON.stringify(initialValue)); 
+    return initialValue; 
+});
+
+const setStoredState = value => {
+    localStorage.setItem(key, JSON.stringify(value)); 
+    setState(value); 
+};
+return [state, setStoredState];
+
+// const [valueLS, setValueLS] = useState(() => {
+//           const item = localStorage.getItem(key);
+//           return item ? JSON.parse(item) : initialValue;
+//         });
+//         const setStoredValue = value => {
+//           const stringifiedValue = JSON.stringify(initialValue);
+//           // persist the stringified value in local storage
+//           localStorage.setItem(key, stringifiedValue);
+//           // put it in state using the setValueLS above
+//           setValueLS(value);
+//         };
+//         return [valueLS, setStoredValue];
+
+};
+
+export default useLocalStorage; 
+
+
+// export function useLocalStorage(key, value) {
+//     const [valueLS, setValueLS] = useState(() => {
+//       const item = window.localStorage.getItem(key)
+//       return item ? JSON.parse(item) : value
+//     })
+//     const setStoredValue = value => {
+//       const stringifiedValue = JSON.stringify(value)
+//       // persist the stringified value in local storage
+//       window.localStorage.setItem(key, stringifiedValue)
+//       // put it in state using the setValueLS above
+//       setValueLS(value)
+//     }
+//     return [valueLS, setStoredValue]
+//   }
